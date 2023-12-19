6544e2b54a19f90751056b6c2dae65231087a09f
Nicolas Beddows <nicolas.beddows@gmail.com>
2023-12-20 10:45:41 +1100

    tidy: move add target and module compilation into separate cmake includes
    these includes are used from both the main build and the sdk build CMakelists

---
+++ CMakeIncludes/addTarget.cmake
@@ -0,0 +1,37 @@
+function(add_target name type module_files source_files)
+    SOURCE_GROUP("Module Files" FILES ${module_files})
+    SOURCE_GROUP("Source Files" FILES ${source_files})
+
+    if(${type} MATCHES EXE)
+        add_executable(${name}
+            ${module_files}
+            ${source_files}
+    )
+    else()
+        add_library(${name} ${type}
+            ${module_files}
+            ${source_files}
+        )
+    endif()
+
+    if(CMAKE_COMPILER_IS_GNUCXX)
+        if(${type} MATCHES EXE)
+            set(cachePath ${CMAKE_BINARY_DIR}/Tests/gcm.cache)
+        else()
+            target_compile_options(${name} PRIVATE -x c++)
+            set(cachePath ${CMAKE_BINARY_DIR}/gcm.cache)
+        endif()
+
+        # Workaround for not being able to set the search path
+        # for the modules. We create a symlink to it from the
+        # current directory.
+        add_custom_target(
+            ${name}ModuleSymlink
+            COMMAND ${CMAKE_COMMAND} -E create_symlink
+            ${cachePath}
+            gcm.cache
+        )
+
+        add_dependencies(${name} ${name}ModuleSymlink)
+    endif()
+endfunction()
