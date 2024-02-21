ccbe7b8fd3dddadaf9d5ff538a5f2d487a3402a4
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-02-22 09:01:12 +1100

    feat: python module
    wrap mach-emu via pybind and output a python module

---
+++ MachinePy/include/MachinePy/MachineHolder.h
@@ -0,0 +1,19 @@
+#include "Machine/MachineFactory.h"
+
+namespace MachEmu
+{
+    // Factory and Machine wrapper
+    class MachineHolder final
+    {
+    private:
+        std::unique_ptr<MachEmu::IMachine> machine_;
+    public:
+        MachineHolder();
+
+        uint64_t Run(uint16_t offset);
+        
+        ErrorCode SetClockResolution(int64_t clockResolution);
+        void SetIoController(MachEmu::IController* controller);
+        void SetMemoryController(MachEmu::IController* controller);
+    };
+} // namespace MachEmu
