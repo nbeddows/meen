a981022231c77aad417a34e5bd43e3b95383b763
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-02-22 18:22:49 +1100

    tidy: don't import entire mach emu module

--- Tests/MachineTest/source/test_Machine.py
+++ Tests/MachineTest/source/test_Machine.py
@@ -1,16 +1,18 @@
 import re
 import unittest
 
-import MachEmuPy as MachEmu
+from MachEmuPy import __version__
+from MachEmuPy import ErrorCode
+from MachEmuPy import Make8080Machine
 
 class MachineTest(unittest.TestCase):
     def setUp(self):
-        self.machine = MachEmu.Make8080Machine()
+        self.machine = Make8080Machine()
         err = self.machine.SetClockResolution(-1)
-        self.assertEqual(err, MachEmu.ErrorCode.NoError)
+        self.assertEqual(err, ErrorCode.NoError)
 
     def test_Version(self):
-        self.assertTrue(re.match(r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$', MachEmu.__version__))
+        self.assertTrue(re.match(r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$', __version__))
 
     def test_SetNoIoController(self):
         with self.assertRaises(ValueError):
@@ -22,7 +24,7 @@ class MachineTest(unittest.TestCase):
 
     def test_BadClockResolution(self):
         err = self.machine.SetClockResolution(0)
-        self.assertEqual(err, MachEmu.ErrorCode.ClockResolution)
+        self.assertEqual(err, ErrorCode.ClockResolution)
 
 if __name__ == '__main__':
     unittest.main()
