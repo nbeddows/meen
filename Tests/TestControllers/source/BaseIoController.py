b13a0e1e9b9f1e582e55bb5038cec7da358c5b1e
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-02-22 16:50:20 +1100

    tidy: clean up import statements

--- Tests/TestControllers/source/BaseIoController.py
+++ Tests/TestControllers/source/BaseIoController.py
@@ -1,18 +1,20 @@
-import MachEmuPy as MachEmu
+from MachEmuPy import Controller
+from MachEmuPy import ISR
 
-class BaseIoController(MachEmu.Controller):
+
+class BaseIoController(Controller):
     def __init__(self):
         super().__init__()
-        self.__isr = MachEmu.ISR.NoInterrupt
+        self.__isr = ISR.NoInterrupt
 
     def ServiceInterrupts(self, currTime, cycles):
         isr = self.__isr
 
-        if self.__isr == MachEmu.ISR.Quit:
-            self.__isr = MachEmu.ISR.NoInterrupt
+        if self.__isr == ISR.Quit:
+            self.__isr = ISR.NoInterrupt
 
         return isr
     
     def Write(self, port, value):
         if port == 0xFF:
-            self.__isr = MachEmu.ISR.Quit
+            self.__isr = ISR.Quit
