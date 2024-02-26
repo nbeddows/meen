b29223e67b063ef86acc285a7da0e8506c51a19f
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-02-26 15:12:39 +1100

    style: use numpy module for mem allocation and rom file reading

--- Tests/TestControllers/source/MemoryController.py
+++ Tests/TestControllers/source/MemoryController.py
@@ -1,23 +1,21 @@
 from MachEmuPy import Controller
 from MachEmuPy import ISR
+import numpy as np
 
 class MemoryController(Controller):
     def __init__(self):
         super().__init__()
         # 16 bit addressable memory
-        self.__memory = [0]*65536
+        self.__memory = np.zeros(65536, dtype=np.uint8)
 
     def Load(self, romFile, offset):
         file = open(romFile, "rb")
-
-        data = file.read(1)
-
-        while data:
-            self.__memory[offset] = data
-            data = file.read(1)
-            offset += 1
-
+        data = np.fromfile(file, dtype=np.uint8)
         file.close()
+
+        for i in range(data.size):
+            self.__memory[offset] = data[i]
+            offset += 1
 
     def Read(self, addr):
         return self.__memory[addr]
