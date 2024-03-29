c13b7324c983945c9f8bfd20708ba9034e2d47b6
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-03-29 12:52:11 +1100

    fix: add uuid to cpu state, more readable json formatting

--- Cpu/source/8080.cpp
+++ Cpu/source/8080.cpp
@@ -24,6 +24,7 @@ module;
 #include <assert.h>
 
 #include "Base/Base.h"
+#include "Utils/Utils.h"
 
 module _8080;
 
@@ -334,9 +335,11 @@ std::unique_ptr<uint8_t[]> Intel8080::GetState(int* size) const
 
 std::string Intel8080::Save() const
 {
-	char str[118]{};
-	snprintf(str, 118, "{\"name\":\"i8080\",\"registers\":{\"a\":%d,\"b\":%d,\"c\":%d,\"d\":%d,\"e\":%d,\"h\":%d,\"l\":%d,\"s\":%d},\"pc\":%d,\"sp\":%d}",
-		Value(a_), Value(b_), Value(c_), Value(d_), Value(e_), Value(h_), Value(l_), Value(status_), pc_, sp_);
+	auto b64 = Utils::BinToTxt("base64", "none", uuid_.data(), uuid_.size());
+	auto fmtStr = "{\n\t\t\"uuid\":\"%s\",\n\t\t\"registers\":\n\t\t{\n\t\t\t\"a\":%d,\n\t\t\t\"b\":%d,\n\t\t\t\"c\":%d,\n\t\t\t\"d\":%d,\n\t\t\t\"e\":%d,\n\t\t\t\"h\":%d,\n\t\t\t\"l\":%d,\n\t\t\t\"s\":%d\n\t\t},\n\t\t\"pc\":%d,\n\t\t\"sp\":%d\n\t}";
+	auto count = snprintf(nullptr, 0, fmtStr, b64.c_str(), Value(a_), Value(b_), Value(c_), Value(d_), Value(e_), Value(h_), Value(l_), Value(status_), pc_, sp_);
+	std::string str(count + 1, '\0');
+	count = snprintf(str.data(), count + 1, fmtStr, b64.c_str(), Value(a_), Value(b_), Value(c_), Value(d_), Value(e_), Value(h_), Value(l_), Value(status_), pc_, sp_);
 	return str;
 }
 
