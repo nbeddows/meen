3beee3584daf577ff18331f8094d4c2f6235b4ee
Nicolas Beddows <nicolas.beddows@gmail.com>
2024-05-27 12:33:06 +1000

    tidy: remove unused image and toolchain files

--- Tools/arm-linux-gnueabihf.cmake
+++
@@ -1,5 +0,0 @@
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR Arm)
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-set(CMAKE_FIND_ROOT_PATH ${CMAKE_SOURCE_DIR}/Arm)
