640316014b19bb6b52c6dd70ac4a3aeab21403aa
Nicolas Beddows <nicolas.beddows@gmail.com>
2025-02-03 16:41:01 +1100

    fix: remove superfluous memory controller Load method

--- tests/source/test_controllers_py/TestControllersPy.cpp
+++ tests/source/test_controllers_py/TestControllersPy.cpp
@@ -12,10 +12,6 @@ PYBIND11_MODULE(TestControllersPy, TestControllers)
     py::class_<meen::MemoryController, meen::IController>(TestControllers, "MemoryController")
         .def(py::init<>())
         .def("Clear", &meen::MemoryController::Clear)
-        .def("Load", [](meen::MemoryController& controller, const char* romFilePath, uint16_t offset)
-        {
-            return controller.Load(romFilePath, offset).value();
-        })
         .def("Read", &meen::MemoryController::Read)
         .def("Write", &meen::MemoryController::Write)
         .def("ServiceInterrupts", &meen::MemoryController::ServiceInterrupts);
